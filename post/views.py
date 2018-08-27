1696f135149e53d999701eb277b93b79f6db07cd
Seamile <lanhuermao@gmail.com>
2018-08-27 10:04:34 +0800

    1. 添加了搜索功能 2. 添加了帖子删除功能 3. 添加了文章分页

--- post/views.py
+++ post/views.py
@@ -1,6 +1,20 @@
+from math import ceil
+
 from django.shortcuts import render, redirect
 
 from post.models import Post
+
+
+def post_list(request):
+    page = int(request.GET.get('page', 1))  # 当前页码
+    per_page = 10  # 每页显示的文章数
+    start = (page - 1) * per_page  # 开始的文章
+    end = start + per_page
+    posts = Post.objects.all()[start:end]
+    total = Post.objects.count()    # 总文章数
+    pages = ceil(total / per_page)  # 总页数
+    return render(request, 'post_list.html',
+                  {'posts': posts, 'pages': range(pages)})
 
 
 def create_post(request):
@@ -33,4 +47,12 @@ def read_post(request):
 
 
 def delete_post(request):
-    return render(request, 'delete_post.html', {})
+    post_id = int(request.GET.get('post_id'))
+    Post.objects.get(pk=post_id).delete()
+    return redirect('/')
+
+
+def search(request):
+    keyword = request.POST.get('keyword')
+    posts = Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts': posts})
